c6441498fbdf100d7a0c8a03e667519e560d292a
Tooba Farooq <161237786+Tooba-Farooq@users.noreply.github.com>
2024-04-09 04:16:27 +0500

    Added unit converter

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ name = None
 
 
 # Function to perform all tasks from fetching data to displaying it
-def fetch_display_weather(city1):
+def fetch_display_weather(city1, combobox_value):
     global name
 
     # Custom exception class for local_data not fetched
@@ -26,6 +26,7 @@ def fetch_display_weather(city1):
         weather_data = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?q={city2}&units=imperial&APPID={api_key}")
         local_data = weather_data.json()
+        print(local_data)
 
         # if user entered invalid location for that raising custom error so that I can come out of
         # fetch_display_weather function
@@ -66,13 +67,17 @@ def fetch_display_weather(city1):
             return dirs[round((d % 360) / 22.5)]
 
         # function to convert fetched sunset and sunrise time from unix timestamp format to 4:00 PM format
-        def convert_unix_timestamp_to_time(unix_timestamp):
-            datetime_object = datetime.fromtimestamp(unix_timestamp)
-            return datetime_object.strftime("%I:%M %p")
+        def convert_unix_timestamp_to_time(unix_timestamp, timezone_offset):
+            local_timezone = pytz.FixedOffset(timezone_offset / 60)  # Convert offset to minutes
+            local_datetime = datetime.fromtimestamp(unix_timestamp, local_timezone)
+            return local_datetime.strftime("%I:%M %p")
 
         # function to calculate dew point
-        def calculate_dewpoint(temperature_fahrenheit, humidity1):
-            temperature_celsius = (temperature_fahrenheit - 32) * 5 / 9
+        def calculate_dewpoint(temperature, humidity1, local_temp_unit):
+            if local_temp_unit == "°F":
+                temperature_celsius = (temperature - 32) * 5 / 9
+            else:
+                temperature_celsius = temperature
 
             # Applying the Magnus formula
             a = 17.27
@@ -81,28 +86,46 @@ def fetch_display_weather(city1):
             dew_point_celsius = (b * alpha) / (a - alpha)
 
             # Convert dew point back to Fahrenheit
-            dew_point_fahrenheit = dew_point_celsius * 9 / 5 + 32
+            if local_temp_unit == "°F":
+                dew_point_fahrenheit = dew_point_celsius * 9 / 5 + 32
+                return round(dew_point_fahrenheit)
+            else:
+                return round(dew_point_celsius)
 
-            return round(dew_point_fahrenheit)
+        def temp_conversion(f):
+            c = (f-32) * 5 / 9
+            return c
+
+        if combobox_value == "Metric: °C, m/s":
+            temp = round(temp_conversion(round(local_data["main"]["temp"])))
+            temp_unit = "°C"
+            temp_max = round(temp_conversion(round(local_data["main"]["temp_max"])))
+            temp_min = round(temp_conversion(round(local_data["main"]["temp_min"])))
+            feels_like = round(temp_conversion(round(local_data["main"]["feels_like"])))
+            wind_speed = round((round(local_data["wind"]["speed"], 1))*0.44704, 1)
+            wind_speed_unit = "m/s"
+        else:
+            temp = round(local_data["main"]["temp"])
+            temp_unit = "°F"
+            temp_max = round(local_data["main"]["temp_max"])
+            temp_min = round(local_data["main"]["temp_min"])
+            feels_like = round(local_data["main"]["feels_like"])
+            wind_speed = round(local_data["wind"]["speed"], 1)
+            wind_speed_unit = "mi/h"
 
         # defining variables
         name = local_data["name"]
-        temp = round(local_data["main"]["temp"])
         weather = local_data["weather"][0]["main"]
         description = local_data["weather"][0]["description"]
-        temp_max = round(local_data["main"]["temp_max"])
-        temp_min = round(local_data["main"]["temp_min"])
-        feels_like = round(local_data["main"]["feels_like"])
-        wind_speed = round(local_data["wind"]["speed"], 1)
         wind_direction_deg = local_data["wind"]["deg"]
         wind_direction_cardinal = degrees_to_cardinal(wind_direction_deg)
         humidity = local_data["main"]["humidity"]
         visibility = f"{(local_data["visibility"]) / 1000}km"
         air_pressure = local_data["main"]["pressure"]
-        dew_point = f"{calculate_dewpoint(temp, humidity)}°"
+        dew_point = calculate_dewpoint(temp, humidity, temp_unit)
         cloud_cover = local_data["clouds"]["all"]
-        sunrise_time = convert_unix_timestamp_to_time(local_data["sys"]["sunrise"])
-        sunset_time = convert_unix_timestamp_to_time(local_data["sys"]["sunset"])
+        sunrise_time = convert_unix_timestamp_to_time(local_data["sys"]["sunrise"], local_data['timezone'])
+        sunset_time = convert_unix_timestamp_to_time(local_data["sys"]["sunset"], local_data['timezone'])
 
         # putting variables in labels
         (Label(master=main_frame, text=name, bg=frame_colour, fg="white", font=("Arial", 24, "bold"))
@@ -113,7 +136,7 @@ def fetch_display_weather(city1):
          .place(relx=0.5, rely=0.155, anchor=CENTER))
         (Label(master=main_frame, text=f"{temp}", bg=frame_colour, fg="white", font=("Times New Roman", 100))
          .place(relx=0.5, rely=0.284, anchor=CENTER))
-        (Label(master=main_frame, text="°F", bg=frame_colour, fg="white", font=("Arial", 30))
+        (Label(master=main_frame, text=temp_unit, bg=frame_colour, fg="white", font=("Arial", 30))
          .place(relx=0.655, rely=0.2))
         (Label(master=main_frame, text=weather, bg=frame_colour, fg="white", font=("Arial", 40))
          .place(relx=0.5, rely=0.42, anchor=CENTER))
@@ -123,12 +146,12 @@ def fetch_display_weather(city1):
                font=("Arial", 16)).place(relx=0.5, rely=0.532, anchor=CENTER))
         (Label(master=main_frame, text="feels like", bg=frame_colour, fg="white",
                font=("Arial", 15, "bold")).place(relx=0.02, rely=0.575))
-        (Label(master=main_frame, text=f"{feels_like}°F", bg=frame_colour, fg="white",
+        (Label(master=main_frame, text=f"{feels_like}{temp_unit}", bg=frame_colour, fg="white",
                font=("Arial", 15)).place(relx=0.055, rely=0.625))
         (Label(master=main_frame, text=f"{wind_direction_cardinal} wind", bg=frame_colour, fg="white",
                font=("Arial", 15, "bold")).place(relx=0.5, rely=0.598, anchor=CENTER))
-        (Label(master=main_frame, text=f"{wind_speed}mi/h {wind_direction_deg}°", bg=frame_colour, fg="white",
-               font=("Arial", 15)).place(relx=0.5, rely=0.646, anchor=CENTER))
+        (Label(master=main_frame, text=f"{wind_speed}{wind_speed_unit} {wind_direction_deg}°", bg=frame_colour,
+               fg="white", font=("Arial", 15)).place(relx=0.5, rely=0.646, anchor=CENTER))
         (Label(master=main_frame, text="Humidity", bg=frame_colour, fg="white",
                font=("Arial", 15, "bold")).place(relx=0.755, rely=0.575))
         (Label(master=main_frame, text=f"{humidity}%", bg=frame_colour, fg="white",
@@ -143,8 +166,8 @@ def fetch_display_weather(city1):
                font=("Arial", 15)).place(relx=0.5, rely=0.766, anchor=CENTER))
         (Label(master=main_frame, text="Dew Point", bg=frame_colour, fg="white",
                font=("Arial", 15, "bold")).place(relx=0.735, rely=0.695))
-        (Label(master=main_frame, text=dew_point, bg=frame_colour, fg="white", font=("Arial", 15))
-         .place(relx=0.825, rely=0.745))
+        (Label(master=main_frame, text=f"{dew_point}{temp_unit}", bg=frame_colour, fg="white", font=("Arial", 15))
+         .place(relx=0.825, rely=0.741))
         (Label(master=main_frame, text="Sunrise", bg=frame_colour, fg="white",
                font=("Arial", 15, "bold")).place(relx=0.02, rely=0.815))
         (Label(master=main_frame, text=sunrise_time, bg=frame_colour, fg="white", font=("Arial", 15))
@@ -166,7 +189,7 @@ def fetch_display_weather(city1):
     bg_img = None
 
     try:
-        # Fetch weather details
+        # Fetching variables from weather_details function
         current_time, current_day, data = weather_details(city1)
 
         # converting current_time from string to datetime object
@@ -199,23 +222,37 @@ def fetch_display_weather(city1):
             bg_img = defining_background_images("images\\night.png")
             frame_colour = "#02041f"
 
-    # Create main frame
+        # Create main frame
         main_frame = Frame(master=bg_img, height=1350, width=400, bg=frame_colour)
         main_frame.pack(anchor=CENTER, padx=8, pady=17)
 
-    # Place labels with weather details
+        # Place labels with weather details
         labels_variables(current_time, current_day, data)
 
     except DataNotFetchedError as ex:
         print("Error fetching weather local_data:", ex)
         print(city1)
-        fetch_display_weather(name)  # calling this function again so screen does not go white
+        fetch_display_weather(name,
+                              combobox_value)  # calling this function again so screen does not go white using name
+        # variable so it keeps showing weather of previous city
         Label(bg_img, text="Invalid locationⓘ", fg="red", font=("Arial", 11)).place(relx=0.9, rely=0.09)
 
+    # Adding function of unit conversion
+    def unit_conversion(combobox_unit):
+        fetch_display_weather(city1, combobox_unit)
+
+    # Adding a unit converter
+    units = ["Imperial: °F, mph", "Metric: °C, m/s"]
+    unit_converter = customtkinter.CTkComboBox(bg_img, values=units, font=("Arial", 12), text_color="grey",
+                                               width=135, border_color="#e3e3e3", button_color="#e3e3e3",
+                                               command=unit_conversion)
+    unit_converter.place(relx=0.78, rely=0.04)
+    unit_converter.set(combobox_value)
+
     # Placing search bar and button
-    search_bar = customtkinter.CTkEntry(master=root, placeholder_text="Search Location")
+    search_bar = customtkinter.CTkEntry(master=root, placeholder_text="Search Location", border_color="#e3e3e3")
     search_bar.place(relx=0.89, rely=0.04)
-    search_bar.bind("<Return>", lambda event: fetch_display_weather(search_bar.get()))
+    search_bar.bind("<Return>", lambda event1: fetch_display_weather(search_bar.get(), unit_converter.get()))
 
 
 # Now making GUI
@@ -232,8 +269,10 @@ try:
     response = requests.get("https://ipinfo.io")
     location_data = response.json()
     city = location_data['city']
-    fetch_display_weather(city)
+    unit = "Imperial: °F, mph"
+    fetch_display_weather(city, unit)
 except Exception as e:
+    unit = "Imperial: °F, mph"
     print("Error", f"Failed to fetch location: {e}")
     customtkinter.CTkLabel(root, text="Your location could not be fetched. However you can search weather of any "
                                       "location you want",  fg_color="white", text_color="black",
@@ -243,6 +282,6 @@ except Exception as e:
     except_search_bar.place(relx=0.2, rely=0.5)
     (customtkinter.CTkButton(master=root, text="Search", bg_color="#b1b3b1", border_width=2,
                              border_color="gray", height=50, corner_radius=5,
-                             command=lambda: fetch_display_weather(except_search_bar.get()))
+                             command=lambda: fetch_display_weather(except_search_bar.get(), unit))
      .place(relx=0.5, rely=0.5))
 root.mainloop()
